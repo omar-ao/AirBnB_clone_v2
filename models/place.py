10479018972ba06a4a74ed4d545b062abfdc85a1
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-20 09:22:54 +0300

    Fix systax error

--- models/place.py
+++ models/place.py
@@ -5,17 +5,17 @@ from sqlalchemy import (Column, String, Integer, Float, ForeignKey)
 from sqlalchemy.orm import relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column('city_id', String(60), nullable=False,
-                     ForeignKey('cities.id'))
-    user_id = Column('user_id', String(60), nullable=False,
-                     ForeignKey('users.id'))
+    city_id = Column('city_id', String(60), ForeignKey('cities.id'),
+                    nullable=False)
+    user_id = Column('user_id', String(60), ForeignKey('users.id'),
+                    nullable=False)
     name = Column('name', String(128), nullable=False)
     description = Column('description', String(1024))
     number_rooms = Column('number_rooms', Integer, nullable=False, default=0)
-    number_bathrooms = Column('number_bathrooms', Integer, nullabel=False,
+    number_bathrooms = Column('number_bathrooms', Integer, nullable=False,
                               default=0)
     max_guest = Column('max_guest', Integer, nullable=False, default=0)
     price_by_night = Column('price_by_night', Integer, nullable=False,
