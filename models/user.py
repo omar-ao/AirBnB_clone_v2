ecbe2505df7b899eea91f521adb11fae00a289ef
Omar Ahmed O <deeyndiid@gmail.com>
2024-02-20 08:47:56 +0300

    update

--- models/user.py
+++ models/user.py
@@ -4,10 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column('email', String(128), nullable=False)
     password = Column('password', String(128), nullable=False)
     first_name = Column('first_name', String(128))
     last_name = Column('last_name', String(128))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
